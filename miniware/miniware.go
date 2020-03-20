1dd272ddbbdbeaac1aaf4d6a6434220b8be391e0
Steve Phillips <steve@tryingtobeawesome.com>
2020-03-20 13:35:43 -0700

    Tweaked Go imports (logrus case-sensitive import path...)

--- miniware/miniware.go
+++ miniware/miniware.go
@@ -10,7 +10,7 @@ import (
 	"sync"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/cathalgarvey/go-minilock/taber"
 	gorillacontext "github.com/gorilla/context"
 	"github.com/gorilla/websocket"
